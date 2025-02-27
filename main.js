65a97e13afbad41a6c418e7e0320563a120d8fe7
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-27 16:15:07 +0100

    select DOM elements, add for loop and generate 10 email with fetch

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/* Descrizione
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
+
+/* select DOM elements */
+const emailListEl = document.getElementById("emailList");
+
 
 const emailList = [];
 
@@ -8,5 +14,10 @@ for(let i = 0; i < 10; i++){
     .then(data => {
         console.log(data.response);
         emailList.push(data.response)
+
+        const email = document.getElementById("li")
+        email.append(data.response)
+        emailListEl.appendChild(email)
     })
 }
+
