a4e5d2a350c0e6ea9f6244958b9926c25a4177d8
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-27 17:12:33 +0100

    fix fetch, add style, add event listener and add comments

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api
 
 /* select DOM elements */
 const emailListEl = document.getElementById("emailList");
+const randomButtonEl = document.querySelector(".random-button")
 
+/* create function to generate 10 random emails */
+function randomEmail(){
 
+/* set empty string to remove the emails on each click */
+emailListEl.innerHTML = "";
+
+/* declare an empty array to push 10 random emails inside */
 const emailList = [];
 
+/* set for loop to iterate 10 times */
 for(let i = 0; i < 10; i++){
         
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
@@ -15,9 +23,21 @@ for(let i = 0; i < 10; i++){
         console.log(data.response);
         emailList.push(data.response)
 
-        const email = document.getElementById("li")
+        /* create <li> element */
+        const email = document.createElement("li")
+        
+        /* append every email in <li> */
         email.append(data.response)
+
+        /* append every <li> in <ul> */
         emailListEl.appendChild(email)
     })
 }
+}
+
+/* invoke function */
+randomEmail()
+
+/* on button's click generate other 10 email */
+randomButtonEl.addEventListener("click", randomEmail);
 
